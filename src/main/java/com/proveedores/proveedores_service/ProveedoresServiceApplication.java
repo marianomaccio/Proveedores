88d93345418e37712791aaa34bc735a8e1708c24
marianomaccio <marianomaccio93@gmail.com>
2025-05-06 16:25:48 +0200

    Creando Api Proveedores con su base de datos

---
+++ src/main/java/com/proveedores/proveedores_service/ProveedoresServiceApplication.java
@@ -0,0 +1,13 @@
+package com.proveedores.proveedores_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProveedoresServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveedoresServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO proveedores (id_proveedor, nombre, fecha_de_alta, id_cliente) VALUES
+(1, 'Coca-cola', '2022-01-01', 5),
+(2, 'Pepsi', '2022-01-01', 5),
+(3, 'Redbull', '2022-01-01', 6);
